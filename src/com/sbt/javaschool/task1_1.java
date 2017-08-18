b2c49aa2a51556a067ce65ef9b2011c61c47d03a
Karen Kazaryan <kazark@list.ru>
2017-08-18 22:17:27 +0300

    Task 1.1 is solved.

---
+++ src/com/sbt/javaschool/task1_1.java
@@ -0,0 +1,21 @@
+package com.sbt.javaschool;
+
+import java.util.Scanner;
+
+public class task1_1 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        //  Read data
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        //  Calc sum
+        int sum = a + b;
+
+        // Write result
+        System.out.println(sum);
+    }
+
+}
