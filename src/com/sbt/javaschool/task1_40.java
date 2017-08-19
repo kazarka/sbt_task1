0d81c4cd3c352e626e4de158f0c1d7528fa8b1c6
Karen Kazaryan <kazark@list.ru>
2017-08-19 22:07:28 +0300

    Task 1.40 is solved.

---
+++ src/com/sbt/javaschool/task1_40.java
@@ -0,0 +1,30 @@
+package com.sbt.javaschool;
+
+import java.util.Scanner;
+
+public class task1_40 {
+
+    public static void main(String[] strings) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String str = scanner.nextLine();
+
+        String minCycleShiftStr = str;
+
+        for (int i = 0; i < str.length(); ++i) {
+            //  Splitting string in two
+            String shiftSubstr1 = str.substring(0,i);
+            String shiftSubstr2 = str.substring(i);
+            //  Concat vice versa
+            String resStr = shiftSubstr2.concat(shiftSubstr1);
+            //  Comparing strings
+            if (resStr.compareTo(minCycleShiftStr) < 0)
+                minCycleShiftStr = resStr;
+        }
+
+        System.out.println(minCycleShiftStr);
+
+    }
+
+}
