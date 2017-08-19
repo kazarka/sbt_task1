2988d249019e87a32d69cdc2a37a9f8758a98554
Karen Kazaryan <kazark@list.ru>
2017-08-19 17:40:06 +0300

    Task 1.37 is solved.

---
+++ src/com/sbt/javaschool/task1_37.java
@@ -0,0 +1,27 @@
+package com.sbt.javaschool;
+
+import java.util.Scanner;
+import java.util.StringJoiner;
+
+public class task1_37 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        //  Reading data
+        String str = scanner.nextLine();
+        int num = Integer.parseInt(scanner.nextLine());
+
+        //  Result
+        StringJoiner sj = new StringJoiner(",");
+
+        //  Join appropriate strings
+        for (String word: str.split(",",-1))
+            if (word.length() >= num)
+                sj.add(word);
+
+        System.out.println(sj);
+    }
+
+}
