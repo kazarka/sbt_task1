bdbab149dd71d24fb4f8008420337672664ff771
Karen Kazaryan <kazark@list.ru>
2017-08-19 13:24:27 +0300

    Task 1.21 is solved.

---
+++ src/com/sbt/javaschool/task1_21.java
@@ -0,0 +1,59 @@
+package com.sbt.javaschool;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
+public class task1_21 {
+
+    /**
+     *  Finds max elements in array and returns it's indices
+     * @param arr Array to analyze
+     * @return List of max elements indices of array
+     */
+    private static List<Integer> getMaxElementsIndices(int[] arr) {
+        List<Integer> maxElemIndices = new LinkedList<>();
+
+        int max = Integer.MIN_VALUE;
+
+        for (int i = 0; i < arr.length; ++i) {
+
+            if (arr[i] > max) {
+                maxElemIndices.clear();
+                max = arr[i];
+            }
+
+            if (arr[i] >= max)
+                maxElemIndices.add(i);
+
+        }
+
+        return maxElemIndices;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        //  Getting data
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; ++i) {
+            arr[i] = scanner.nextInt();
+        }
+
+        //  Twicely dividing max elements
+        for (int i = 1; i <= 2; ++i) {
+
+            for (Integer ind : getMaxElementsIndices(arr)) {
+                arr[ind] /= 2;
+            }
+
+        }
+
+        //  Print result array
+        System.out.println(Arrays.toString(arr).replaceAll("[\\[,\\]]", ""));
+    }
+
+}
