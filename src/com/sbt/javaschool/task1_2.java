5cb3a21560cd28355c72ee278e8016f6027d9d77
Karen Kazaryan <kazark@list.ru>
2017-08-20 19:31:26 +0300

    Task 1.2 is solved.

---
+++ src/com/sbt/javaschool/task1_2.java
@@ -0,0 +1,20 @@
+package com.sbt.javaschool;
+
+import java.util.Scanner;
+
+public class task1_2 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int count = scanner.nextInt();
+        int sum = 0;
+
+        for (int i = 0; i < count; ++i)
+            sum += scanner.nextInt();
+
+        System.out.println(sum);
+    }
+
+}
