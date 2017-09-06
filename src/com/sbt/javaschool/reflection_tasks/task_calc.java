491ca283e8df2c39ad8eb663e152cc59278562f0
Karen Kazaryan <kazark@list.ru>
2017-09-06 07:53:54 +0300

    Added reflection_tasks. Added calculator

---
+++ src/com/sbt/javaschool/reflection_tasks/task_calc.java
@@ -0,0 +1,11 @@
+package com.sbt.javaschool.reflection_tasks;
+
+public class task_calc {
+
+    public static void main(String[] args) {
+
+        System.out.println(Calculator.calc("\n2.7 + \r\n  3.6\t"));
+
+    }
+
+}
